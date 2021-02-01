db0eb7e7b4cf2a44775fa1614f57bafe7a06128c
JonasDM <jonasdemeyer@outlook.com>
2021-02-01 11:35:31 +0100

    added fonts

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "Images/Png/logo.png";
 
 function App() {
   return (
     <div className="App">
-      <div className="c-header__homepage"></div>
+      <div className="c-header">
+        <div className="c-header__homepage"></div>
+        <img className="c-header__logo" src={logo} alt="logo-kaj" />
+      </div>
     </div>
   );
 }
