c6b0b7e3b6880f1dda4211d9ae4c4accab27834a
JonasDM <jonasdemeyer@outlook.com>
2021-02-06 14:45:00 +0100

    delete redundant files

--- src/Pages/Afdelingen/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Afdelingen() {
-  return <div></div>;
-}
--- src/Pages/OverOns/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function OverOns() {
-  return <div></div>;
-}
