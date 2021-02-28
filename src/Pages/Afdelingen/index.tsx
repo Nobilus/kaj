a9517ac47711f3dae5116296484d94cc4e35934f
JonasDM <jonasdemeyer@outlook.com>
2021-02-28 14:37:42 +0100

    fix usercard grid

--- src/Pages/Afdelingen/index.tsx
+++ src/Pages/Afdelingen/index.tsx
@@ -1,9 +1,11 @@
+import PageDivider from "Components/PageDivider";
 import { LatLngExpression, divIcon } from "leaflet";
 import React, { useEffect, useMemo, useState } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import { colors } from "Utils/colors";
 import IAfdeling from "Utils/Types/afdelingen";
+import OnsTeamIcon from "Images/Png/ons_team_icon.png";
 
 function Afdelingen() {
   const afdelingen: Array<IAfdeling> = require("Assets/Lokalen.json");
@@ -44,27 +46,30 @@ function Afdelingen() {
   };
 
   return (
-    <MapContainer
-      className="c-map-container"
-      center={position}
-      zoom={12}
-      scrollWheelZoom={true}
-    >
-      <ChangeView center={position} zoom={12} />
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {afdelingen.map((item) => (
-        <Marker position={item.coords} icon={customMarkerIcon}>
-          <Popup>
-            {item.naam} <br />
-            {item["e-mail"]} <br />
-            {item.tel}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      <PageDivider src={OnsTeamIcon} alt={""} title={"Afdelingen"} />
+      <MapContainer
+        className="c-map-container"
+        center={position}
+        zoom={12}
+        scrollWheelZoom={true}
+      >
+        <ChangeView center={position} zoom={12} />
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {afdelingen.map((item) => (
+          <Marker position={item.coords} icon={customMarkerIcon}>
+            <Popup>
+              {item.naam} <br />
+              {item["e-mail"]} <br />
+              {item.tel}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
   );
 }
 
