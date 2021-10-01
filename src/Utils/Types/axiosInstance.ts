44dbb8b778adfc0b501131f5316492e06740c1fd
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-10-01 10:44:58 +0200

    fix https

--- src/Utils/Types/axiosInstance.ts
+++ src/Utils/Types/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosI = axios.create({
-  baseURL: "http://wordpress.kaj.be/wp-json/",
+  baseURL: "https://wordpress.kaj.be/wp-json/",
   headers: {
     authorization:
       "Basic d2Vic2l0ZXVzZXI6dVZNNSBSRjRjIDUyU1kga20xeCBrV2JLIG5pcng=",
