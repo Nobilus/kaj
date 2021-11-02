16763a8456b713db761440b2b5c81e31dc372b6c
JonasDM <43569205+Jonas-D-M@users.noreply.github.com>
2021-11-02 17:15:50 +0100

    make footer editable

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import parse from "html-react-parser";
 //socials icons
 import facebook from "Images/Png/socials/facebook.png";
 import instagram from "Images/Png/socials/instagram.png";
@@ -8,19 +8,68 @@ import twitter from "Images/Png/socials/twitter.png";
 // sponsors
 import dvv from "Images/Png/sponsors/logo-dvv.png";
 import vlaanderen from "Images/Png/Vlaanderen_verbeelding_werkt.png";
+import { AxiosRequestConfig } from "axios";
+import endpoints from "Utils/endpoints";
+import { axiosI } from "Utils/Types/axiosInstance";
+import { IPage } from "Utils/Types/page";
 
 function Footer() {
+  const [page, setPage] = useState<IPage>();
+  const [address, setAddress] = useState("");
+  const [images, setImages] = useState("");
+  useEffect(() => {
+    const fetchPage = async () => {
+      const axiosconf: AxiosRequestConfig = {
+        params: {
+          slug: "footer",
+          _embed: true,
+        },
+      };
+      axiosI
+        .get<Array<IPage>>(endpoints.pagebyslug, axiosconf)
+        .then(({ data }) => {
+          console.log(data[0]);
+
+          setPage(data[0]);
+        })
+        .catch(() => {});
+    };
+    fetchPage();
+  }, []);
+
+  useEffect(() => {
+    console.log(address);
+
+    return () => {};
+  }, [address]);
+
+  useEffect(() => {
+    if (page) {
+      const content = page.content.rendered;
+      const imageBegin = content.indexOf("<p>") + 3;
+
+      const tempAddress = content.substring(0, imageBegin - 3);
+      const tempImages = content.substring(
+        imageBegin,
+        content.lastIndexOf("</p>")
+      );
+      setAddress(tempAddress);
+      setImages(tempImages);
+    }
+  }, [page]);
+
   return (
     <footer className="o-row__footer">
       <h4 className="c-footer-title">KAJ VZW</h4>
       <div className="c-footer-row">
         <div className="c-footer-row__column">
-          <p className="c-footer-row__item">Britsierlaan 5</p>
+          {/* <p className="c-footer-row__item">Britsierlaan 5</p>
           <p className="c-footer-row__item">1030 Schaarbeek</p>
           <p className="c-footer-row__item">
             Tel: <a href="tel:022465300">02/246.53.00</a>
-          </p>
-          <p className="c-footer-row__item">info@kaj.be</p>
+          </p> */}
+          {/* <p className="c-footer-row__item">info@kaj.be</p> */}
+          <div className="c-address">{parse(address)}</div>
         </div>
         <div className="c-footer-row__column c-footer-row__column-row">
           <a
@@ -51,7 +100,7 @@ function Footer() {
         className="c-footer-row"
         style={{ justifyContent: "center", marginBottom: 16 }}
       >
-        <img style={{ height: 48 }} src={vlaanderen} alt="vlaanderen" />
+        {parse(images)}
       </div>
       <div className="c-attribution-container">
         <p className="c-attribution">
